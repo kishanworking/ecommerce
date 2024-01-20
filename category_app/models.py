97a8e0b92901a627b38b33b425e090bb5790e8f6
kishan <156877941+Kishandubey1910@users.noreply.github.com>
2024-01-20 17:12:03 +0530

    category

--- category_app/models.py
+++ category_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
@@ -11,6 +11,10 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+
+    # takeing url of perticular category 
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.category_name
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -14,19 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
-from . import views
 from django.conf.urls.static import static 
 from django.conf import settings
-
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', views.home, name="home"),
 
+    # linking to store app urls.py
+    path('store/', include('store_app.urls')),
+
+
+    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
